2bebbd3de16821ba3ca914d9820b9c30d08b0952
REYESVA <vance.albert.reyes@oocl.com>
2020-10-25 13:39:37 +0800

    [Spring JPA][Bryan-Vance]feature - add company response and company request

---
+++ src/main/java/com/thoughtworks/springbootemployee/dto/CompanyRequest.java
@@ -0,0 +1,32 @@
+package com.thoughtworks.springbootemployee.dto;
+
+import com.thoughtworks.springbootemployee.model.Employee;
+
+import java.util.List;
+
+public class CompanyRequest {
+    private String companyName;
+    private List<Employee> employees;
+
+    public CompanyRequest(String companyName, List<Employee> employees) {
+        this.companyName = companyName;
+        this.employees = employees;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public List<Employee> getEmployees() {
+        return employees;
+    }
+
+    public void setEmployees(List<Employee> employees) {
+        this.employees = employees;
+    }
+}
+
